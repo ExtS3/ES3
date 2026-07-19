1692b2cb806b3ebbd249d4d48a3a3ba549e858f6
Juhyeok0603 <juhyeok20603@gmail.com>
2026-07-19 23:22:07 +0900

    fix: 업로드창 화살표 수정

--- ExtS3-Web-UI/frontend/static/js/common.js
+++ ExtS3-Web-UI/frontend/static/js/common.js
@@ -71,7 +71,7 @@ if (sideBottom) {
     <button id="slackConnect" class="es3-btn w-full" style="background:#4A154B">
         <svg width="15" height="15" viewBox="0 0 122.8 122.8" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#E01E5A"/>
-            <path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8w 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36C5F0"/>
+            <path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36C5F0"/>
             <path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2EB67D"/>
             <path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z" fill="#ECB22E"/>
         </svg>
@@ -79,10 +79,10 @@ if (sideBottom) {
     </button>
 </div>
 <div class="px-4 pb-3">
-    <button id="moveBuild" class="es3-btn w-full">
+    <a id="moveBuild" href="/build" class="es3-btn w-full">
         <span class="material-symbols-outlined text-sm" data-icon="upload">upload</span>
         Upload
-    </button>
+    </a>
 </div>
 <div class="px-4 pb-4 flex flex-col gap-1 border-t border-outline-variant pt-3">
     <a id="user" class="flex items-center gap-3 px-3 py-2.5 rounded-lg text-sm font-medium transition-colors text-primary hover:bg-primary-container" href="/login">
